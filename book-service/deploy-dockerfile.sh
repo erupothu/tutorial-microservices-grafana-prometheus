ec28ea675338f628271c0c4605873f515d9ecb87
Erupothu Harish <erupothu@users.noreply.github.com>
2020-07-16 23:17:19 +0530

    Update deploy-dockerfile.sh

--- book-service/deploy-dockerfile.sh
+++ book-service/deploy-dockerfile.sh
@@ -8,4 +8,4 @@ mvn package
 docker stop tutorial-microservices-grafana-prometheus || true && docker rm tutorial-microservices-grafana-prometheus || true
 docker rmi tutorial-microservices-grafana-prometheus || true
 docker build -f Dockerfile -t tutorial-microservices-grafana-prometheus .
-docker run --name tutorial-microservices-grafana-prometheus -p 8081:8081 -d data-warehouse:latest
+docker run --name tutorial-microservices-grafana-prometheus -p 8081:8081 -d tutorial-microservices-grafana-prometheus:latest
